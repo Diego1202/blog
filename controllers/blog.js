bf5502e897cc86a49db5e47dcd9525decba4493c
unknown <cedenod189@gmail.com>
2024-04-18 10:20:25 -0500

    Add a new endpoints for user schema and login using json Web Token

---
+++ controllers/blog.js
@@ -0,0 +1,71 @@
+const { Router } = require('express')
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const blogRouter = Router()
+
+const verify = (req, res, next) => {
+	const decodedToken = jwt.verify(req.token, process.env.SECRET)
+	if (!decodedToken.id) return res.status(401).json({ error: 'token invalid' })
+	return decodedToken
+
+}
+
+blogRouter.get('/', async (req, res, next) => {
+	try {
+		const token = verify(req, res, next)
+		await Blog
+			.find({ user: token.id }).populate('user', { username: 1, name: 1 })
+			.then(blogs => res.json(blogs))
+	} catch (error) {
+		next(error)
+	}
+})
+
+blogRouter.get('/:id', async (req, res, next) => {
+	await Blog
+		.findById(req.params.id)
+		.then(blog => {
+			if (blog) {
+				res.status(200).json(blog)
+			} else {
+				res.status(404).json({ message: 'Not found' })
+			}
+		}).catch(error => next(error))
+})
+
+blogRouter.post('/', async (req, res) => {
+	const { userId, title, author, url, likes } = req.body
+
+	const user = await User.findOne({ _id: userId })
+	if (!user) res.status(400).json({ error: 'User not found' })
+
+	const blog = new Blog({
+		title,
+		author,
+		url,
+		likes,
+		user: user._id
+	})
+
+	await blog
+		.save()
+		.then(async (result) => {
+			user.blogs = user.blogs.concat(result._id)
+			await user.save()
+			res.status(201).json(result)
+		}).catch(error => {
+			res.status(400).send({ error })
+		})
+})
+
+blogRouter.delete('/:id', async (req, res, next) => {
+	await Blog
+		.findByIdAndDelete(req.params.id)
+		.then(() => {
+			res.status(204).end()
+		}).catch(error => next(error))
+})
+
+module.exports = blogRouter
---
+++ controllers/users.js
@@ -0,0 +1,28 @@
+const { Router } = require('express')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+const usersRouter = Router()
+
+usersRouter.get('/', async (req, res) => {
+	await User.find().populate('blogs', { title: 1, author: 1, url: 1, likes: 1 }).then(users =>
+		res.status(200).json(users)
+	)
+})
+
+usersRouter.post('/', async (req, res, next) => {
+	const { username, name, password } = req.body
+	if (password.length < 3) return res.status(400).json({ error: 'La contraseña debe tener al menos 3 caracteres' })
+	const passwordHash = await bcrypt.hash(password, 10)
+	const user = new User({
+		username,
+		name,
+		passwordHash
+	})
+	user.save().then(savedUser => {
+		res.status(201).json(savedUser)
+	}).catch(error => next(error))
+
+})
+
+module.exports = usersRouter
